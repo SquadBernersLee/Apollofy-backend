033da7e784db489000520a6d4300b26f9b97a190
alvarodct23 <alvarodct23@gmail.com>
2024-04-17 16:23:45 +0200

    adding .env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { checkJwtMiddlewares } from "./middlewares/checkjwt.middlewares";
+import { requestRoutes } from "./routes/request.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/user");
+app.use("/api", requestRoutes)
 export default app;
