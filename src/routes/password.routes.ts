307e08260d22de76c0556789c68b2947c8e8dbeb
alvarodct23 <alvarodct23@gmail.com>
2024-04-17 19:50:46 +0200

    adding checkJwtMiddlewares pass

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { createPassword, getAllPasswords, updatePassword } from "../controllers/password.controllers";
+import { checkJwtMiddlewares } from "../middlewares/checkjwt.middlewares";
 
 const passwordRouter = Router();
 
-passwordRouter.get("/api/authenticate", getAllPasswords)
-passwordRouter.post("/api/authenticate", createPassword)
-passwordRouter.patch("/api/authenticate:passwordId", updatePassword)
-// passwordRouter.delete("/api/authenticate:passwordId", deletepassword)
+passwordRouter.get("/api/authenticate", checkJwtMiddlewares, getAllPasswords)
+passwordRouter.post("/api/authenticate", checkJwtMiddlewares, createPassword)
+passwordRouter.patch("/api/authenticate:passwordId", checkJwtMiddlewares, updatePassword)
 
 export default passwordRouter;
