d06b4a5e47ee4b675a79b80aaa898fe432542ed3
javier martin ruiz <javiermartinruiz@MacBook-Pro.local>
2024-04-25 13:43:00 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,20 @@ import playlistRouter from "./routes/playlist.routes";
 import { tracksRouter } from "./routes/tracks.routes";
 import { likedTrackRouter } from "./routes/likedTracks.routes";
 import { myTracksRouter } from "./routes/myTracks.routes";
-
+import cors from "cors";
 const app = express();
 
+app.use(
+    cors({
+      origin: process.env.APP_ORIGIN,
+    })
+  );
 app.use(express.json());
 /* app.use("/user"); */
 app.use("/api", requestRoutes);
 app.use("/api/playlist", playlistRouter);
 app.use("/api/track", tracksRouter);
-app.use("/api/track", likedTrackRouter)
-app.use("/api/track", myTracksRouter)
+app.use("/api/likedtrack", likedTrackRouter)
+app.use("/api/mytrack", myTracksRouter)
 
 export default app;
