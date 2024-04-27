65eef2687f466bba27ae5c53bdd265b03bc15b7f
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-04-27 09:59:10 +0200

    save

--- src/routes/playlist.routes.ts
+++ src/routes/playlist.routes.ts
@@ -4,7 +4,12 @@ import {
   createPlaylist,
   deletePlaylist,
   followPlaylist,
+  followUnfollow,
   getAllPlaylist,
+  getFollowedPlaylistsByUserId,
+  getPlaylistById,
+  getSongsByPlaylistId,
+  isUserFollowingPlaylist,
   updatePlaylist,
 } from "../controllers/playlist.controllers";
 
@@ -17,4 +22,17 @@ playlistRouter.delete("/:id", deletePlaylist);
 playlistRouter.post("/:id/follow", followPlaylist);
 
 playlistRouter.post("/:id", addSongPlaylist);
+
+playlistRouter.get("/followedPlaylists/:userId", getFollowedPlaylistsByUserId);
+playlistRouter.get("/songs/:id", getSongsByPlaylistId);
+
+playlistRouter.get("/individual/:id", getPlaylistById);
+
+playlistRouter.get(
+  "/is-following-playlist/:userId/:playlistId",
+  isUserFollowingPlaylist
+);
+
+playlistRouter.get("/followUnfollow/:userId/:id", followUnfollow);
+
 export default playlistRouter;
